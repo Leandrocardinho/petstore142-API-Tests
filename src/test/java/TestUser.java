320ddcd099ddf493bd2efb7322474ca4bf86c961
Leandrocardinho <leandro_jam_pa@hotmail.com>
2025-11-30 12:34:01 -0300

    first commit

---
+++ src/test/java/TestUser.java
@@ -0,0 +1,38 @@
+// 0 - Pacote
+
+
+// 1- bibliotecas
+
+// 2-   classe
+
+import org.junit.jupiter.api.Test;
+
+public class TestUser {
+    // 2.1 atributos
+
+    static String ct = "application/json"; // content -type
+
+    static String uriUser = "https://petstore.swagger.io/v2/swagger.json/user";
+    
+    // 2.2 funçoes e métodos
+    // 2.2.1 funções e métodos comuns / uteis
+
+    // 2.2.2 métodos de teste
+   
+    //  Função de leitura de Json
+    public static String lerArquivoJson(String arquivoJson){
+        //TODO: "Completar a leitura do arquivo"
+        //return new String(Files);
+        return null;
+    }
+
+    @Test
+    public void testPostPet (){
+        // carregar os dados do arquivo json do pet
+        //TODO: Criar o metodo Post
+       // String jsonBody = 
+
+
+    }
+    
+}
